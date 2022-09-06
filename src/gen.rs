5d23449d48d45f91a97d9c257d8f21bc1335f488
mWalrus <hellkvistoskar@protonmail.com>
2022-09-06 22:36:02 +0200

    feat: gen::file_name now generates with an extension

--- src/gen.rs
+++ src/gen.rs
@@ -2,9 +2,10 @@ use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 
 pub fn file_name() -> String {
-    thread_rng()
+    let name: String = thread_rng()
         .sample_iter(&Alphanumeric)
         .take(8)
         .map(char::from)
-        .collect()
+        .collect();
+    format!("{name}.webp")
 }
