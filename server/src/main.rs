590aa1d1accfe4c29077b0a1f87bd56495bf018a
mWalrus <hellkvistoskar@protonmail.com>
2022-09-18 14:50:46 +0000

    feat: defining file size limitations

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,11 +5,19 @@ mod convert_image;
 mod gen;
 mod routes;
 
+use rocket::data::{Limits, ToByteUnit};
+use rocket::Config;
 use routes::{default, internal_error, not_found, too_many_requests};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    let mut config = Config::default();
+
+    let limits = Limits::default().limit("image", 5.mebibytes());
+
+    config.limits = limits;
+
+    rocket::custom(config)
         .register(
             "/",
             catchers![not_found, internal_error, too_many_requests, default],
