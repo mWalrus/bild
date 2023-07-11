34545c737c7def7316ab639466bf1d53bc2325c2
mWalrus <hellkvistoskar@protonmail.com>
2023-07-11 15:59:57 +0200

    feat: add cors

---
+++ server/src/fairings.rs
@@ -0,0 +1,30 @@
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{self, Header, Status};
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, HEAD, DELETE, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+
+        if request.method() == http::Method::Options && request.route().is_none() {
+            response.set_status(Status::NoContent);
+            let _ = response.body_mut().take();
+        }
+    }
+}
